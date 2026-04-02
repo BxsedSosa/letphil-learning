5242c47f0279c4aa3b6491a5c80897511bb28d28
BalatroAP <donovanxabad@gmail.com>
2026-04-02 08:25:33 -0700

    completed challenge

---
+++ level-1-web-dev-foundation/challenges/two-button/script.js
@@ -0,0 +1,7 @@
+let btn1 = document.getElementById("btn-1");
+let btn2 = document.getElementById("btn-2");
+let txt = document.getElementById("img-text");
+
+btn1.addEventListener("input", btn1.innerHTML);
+btn2.addEventListener("input", btn2.innerHTML);
+txt.addEventListener("input", txt.innerHTML);
